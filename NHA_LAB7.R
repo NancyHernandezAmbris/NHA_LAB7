efb965858a327e67e3b6fb516e6a4536d94438b1
Nancy Hernández Ambris <99464343+NancyHernandezAmbris@users.noreply.github.com>
2022-02-17 23:18:46 -0600

    Update NHA_LAB7.R

--- NHA_LAB7.R
+++ NHA_LAB7.R
@@ -10,36 +10,7 @@ Hecho con gusto por NANCY HERNÁNDEZ AMBRIS
 # 3. Conventir un dataframe en matriz
 
 
-########################################
-# LABORATORIO: Gestión de datos de red #
-########################################
 
-Hecho con gusto por Carla Carolina Pérez Hernández (UAEH)
-
-# Laboratorio - Gestión de datos de red - curaduría de datos
-
-# Objetivo: Explorar la organización de los datos
-# ------------------------------------------------------------------------------------------------------
-# En este ejercicio vamos a:
-# 1. Cargar nuestra matriz hipotética de datos
-# 2. Transformar la matriz en un dataframe
-# 3. Conventir un dataframe en matriz
-
-
-########################################
-# LABORATORIO: Gestión de datos de red #
-########################################
-
-Hecho con gusto por Carla Carolina Pérez Hernández (UAEH)
-
-# Laboratorio - Gestión de datos de red - curaduría de datos
-
-# Objetivo: Explorar la organización de los datos
-# ------------------------------------------------------------------------------------------------------
-# En este ejercicio vamos a:
-# 1. Cargar nuestra matriz hipotética de datos
-# 2. Transformar la matriz en un dataframe
-# 3. Conventir un dataframe en matriz
 
 
 ########################################
